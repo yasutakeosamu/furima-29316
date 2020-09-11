de2ec174721777ce6ef8bc1ce523818af57c3f07
yasutakeosamu <036yasutake@gmail.com>
2020-09-11 16:29:49 +0900

    Userモデルのテストコードを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :familyname, :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :familyname_reader, :firstname_reader, presence: true, format: { with: /\A[ァ-ン]+\z/ }

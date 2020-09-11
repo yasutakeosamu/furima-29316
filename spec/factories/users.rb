d53fc753ceea0ec34aa58851187e5312c49e57e5
yasutakeosamu <036yasutake@gmail.com>
2020-09-11 14:08:35 +0900

    FactoryBotにユーザー情報を設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.initials}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6, mix_case: true)
+    password {password}
+    password_confirmation {password}
+    familyname {Faker::Name.last_name}
+    firstname {Faker::Name.first_name}
+    familyname_reader {"カタカナ"}
+    firstname_reader {"カタカナ"}
+    birthday {Faker::Date.between(from: '1930-01-01', to: '2020-08-31')}
   end
 end

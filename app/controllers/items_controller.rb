d53a68e0596027498e02cfaba0fd6a9f71e122cf
yasutakeosamu <036yasutake@gmail.com>
2020-09-14 11:31:30 +0900

    出品ページへの遷移とリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    unless user_signed_in?
+      redirect_to root_path
+    end
     @item = Item.new
   end
   def create

1cc37aaddaa2137dfdda43bd19d07f7a99fa612c
yasutakeosamu <036yasutake@gmail.com>
2020-09-16 16:49:18 +0900

    editアクションとpudateアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
 
   def item_params

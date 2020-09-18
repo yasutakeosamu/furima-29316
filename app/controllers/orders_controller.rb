870f32938766d97de98025236ba74b04bad922a5
yasutakeosamu <036yasutake@gmail.com>
2020-09-18 20:37:12 +0900

    購入ページへの遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
+    redirect_to root_path unless user_signed_in? && @item.user_id != current_user.id
   end
   def create
     

dec3e089a1d2d7501877236fe9be3ea32fbbf659
yasutakeosamu <036yasutake@gmail.com>
2020-09-19 17:13:26 +0900

    商品が購入されているかどうかで表示を変える

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
-  def index
-    @purchase = Purchase.new
+  def index  
     @item = Item.find(params[:item_id])
+    redirect_to root_path if @item.purchase != nil
+    @purchase = Purchase.new
     redirect_to root_path unless user_signed_in? && @item.user_id != current_user.id
   end
   def create

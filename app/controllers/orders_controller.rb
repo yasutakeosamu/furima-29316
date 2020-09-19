3772f1efb682f7f7369e63331896cbb7801d8828
yasutakeosamu <036yasutake@gmail.com>
2020-09-19 16:59:48 +0900

    PAY.JPを導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,33 @@
 class OrdersController < ApplicationController
   def index
+    @purchase = Purchase.new
     @item = Item.find(params[:item_id])
     redirect_to root_path unless user_signed_in? && @item.user_id != current_user.id
   end
   def create
-    
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:item_id, :token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency:'jpy'
+    )
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,7 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :customer
+
+  attr_accessor :token
+  validates :token, :user_id, :item_id, presence:true
 end

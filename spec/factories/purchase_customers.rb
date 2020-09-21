a98dd2d58c366709dd3018a0eb1d2aa4c5286b4e
yasutakeosamu <036yasutake@gmail.com>
2020-09-21 12:41:41 +0900

    購入機能のモデルテスト

---
+++ spec/factories/purchase_customers.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :purchase_customer do
+    token {Faker::Lorem.characters}
+    postal_code {Faker::Address.postcode}
+    state {Faker::Number.between(from:2,to:48)}
+    city {Faker::Address.city}
+    address {Faker::Address.street_address}
+    building {Faker::Address.building_number}
+    phone_number {Faker::Number.number(digits:11)}
+    user_id {Faker::Number.between(from:1,to:5)}
+    item_id {Faker::Number.between(from:1,to:50)}
+  end
+end

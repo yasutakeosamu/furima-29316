dd7223c896e86d5dc240a70a81b6fee10f8f5f48
yasutakeosamu <036yasutake@gmail.com>
2020-09-24 17:08:48 +0900

    カード情報を保存する実装

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,5 +4,20 @@ class CardsController < ApplicationController
   end
 
   def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+      description: 'test',
+      card: params[:token]
+    )
+    @card = Card.new(
+      card_token: params[:token],
+      customer_token: customer.id,
+      user_id: current_user.id
+    )
+    if @card.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ApplicationRecord
   belongs_to :user
+
+  validates :card_token, :customer_token, :user_id, presence:true
 end
